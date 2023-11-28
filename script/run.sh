e346d17df987d84835dcb8eff7ab8afa0b832e92
Yousx <yousx@Yousxs-MacBook-Pro.local>
2023-11-28 17:31:37 +0800

    test

--- script/run.sh
+++ script/run.sh
@@ -11,4 +11,9 @@ python ../src/load_data.py
 echo "Running lora.py..."
 python ../src/lora.py
 
+echo "cover model to llama.cpp..."
+python3 ~/src/llama.cpp convert.py --outfile ~/models/llamacpp/zy-f16.bin --outtype f16 /Users/you/src/llm_demo/outputs/zysft
+~/src/llama.cpp/quantize  ~/models/llamacpp/zy-f16.bin ~/models/llamacpp/zysft-q4.bin q4_0
+mv ~/models/llamacpp/zysft-q4.bin ~/text-generation-webui/models/
+
 echo "All scripts executed successfully."
