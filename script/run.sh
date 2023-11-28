14900c69e8af3a2bb7672520b7c0c9639875957c
Yousx <yousx@Yousxs-MacBook-Pro.local>
2023-11-28 18:11:15 +0800

    one key run

--- script/run.sh
+++ script/run.sh
@@ -1,13 +1,13 @@
 #!/bin/zsh
 
-# echo "Running prepare_data.py..."
-# python ../src/prepare_data.py
+echo "Running prepare_data.py..."
+python ../src/prepare_data.py
 
-# echo "Running load_data.py..."
-# python ../src/load_data.py
+echo "Running load_data.py..."
+python ../src/load_data.py
 
-# echo "Running lora.py..."
-# python ../src/lora.py
+echo "Running lora.py..."
+python ../src/lora.py
 
 echo "cover model to llama.cpp..."
 python3 ~/src/llama.cpp/convert.py --outfile ~/models/llamacpp/zy-f16.bin --outtype f16 /Users/you/src/llm_demo/outputs/zysft
