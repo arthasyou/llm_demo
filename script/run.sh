fc9f201bbb9686eef71827704bb050b08d3e467e
Yousx <yousx@Yousxs-MacBook-Pro.local>
2023-11-28 17:20:22 +0800

    test

--- script/run.sh
+++ script/run.sh
@@ -3,12 +3,12 @@
 # 假设这些 Python 脚本位于当前目录下，或者您可以在下面的命令中指定完整路径
 
 echo "Running prepare_data.py..."
-python prepare_data.py
+python ../src/prepare_data.py
 
 echo "Running load_data.py..."
-python load_data.py
+python ../src/load_data.py
 
 echo "Running lora.py..."
-python lora.py
+python ../src/lora.py
 
 echo "All scripts executed successfully."
