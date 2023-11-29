9c369481db5357d6d30b5e46b25a0d03bd48c410
Yousx <yousx@Yousxs-MacBook-Pro.local>
2023-11-29 13:53:21 +0800

    text

--- src/pt.py
+++ src/pt.py
@@ -28,7 +28,7 @@ def print_trainable_parameters(model):
 # -------------------------------------------------------------
 
 def generate_origin(example):
-    r = example + "<s>" 
+    r = example + "</s>" 
     return r
 
 def format_text(sample):
